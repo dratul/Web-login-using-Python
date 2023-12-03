af173faf9214503daefa49915f9f95e691401f82
Dr Atul Kumar Dwivedi <68795168+dratul@users.noreply.github.com>
2023-12-03 06:21:26 +0530

    updated with relogin after 5 minutes

--- Main.py
+++ Main.py
@@ -4,16 +4,16 @@ import login_details
 import time
 username = login_details.username
 password = login_details.password
-
 driver = webdriver.Edge()
-driver.get("http://172.16.40.5:8090/")
-#driver.maximize_window()
-#driver.get('https://bing.com')
-
-#element = driver.find_element(By.ID, 'username')
-#element.send_keys('balkrishna')
-driver.find_element(By.ID, "username").send_keys(username)
-driver.find_element(By.ID, "password").send_keys(password)
-driver.find_element(By.ID,"loginbutton").click()
-time.sleep(5)
-#driver.quit()
+driver.maximize_window()
+   #
+    #driver.get('https://bing.com')
+    #element = driver.find_element(By.ID, 'username')
+    #element.send_keys('balkrishna')
+while(True):
+    driver.get("http://172.16.40.5:8090/")
+    driver.find_element(By.ID, "username").send_keys(username)
+    driver.find_element(By.ID, "password").send_keys(password)
+    driver.find_element(By.ID,"loginbutton").click()
+    time.sleep(300)
+    #driver.quit()
